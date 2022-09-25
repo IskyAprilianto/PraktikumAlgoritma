8f6af4ef87dce459aae4b944e3120e95984e02b8
IskyAprilianto <113821298+IskyAprilianto@users.noreply.github.com>
2022-09-25 12:29:45 +0700

    Add files via upload

---
+++ algoritma h2 tugas 1.py
@@ -0,0 +1,26 @@
+#Isky Dwi Aprilianto
+import math
+a = int(input("masukan nilai a :"))
+b = int(input("masukan nilai b :"))
+#penjumlahan
+print("=================")
+print("hasil penjumlahan :", a+b)
+#pengurangan
+print("=================")
+print("sisa hasil pengurangan:", abs(b-a))
+#perkalian
+print("=================")
+print("hasil perkalian", a*b)
+#pembagian
+print("=================")
+print("hasil pembagian :", a/b)
+#sisapembagian
+print("=================")
+print("selisih pembagian : ", abs(a/b))
+#log(a)
+print("=================")
+print("log(a)")
+print("hasil algoritma :", math.log10(a))
+#pangkat
+print("=================")
+print("hasil pangkat", a**b)

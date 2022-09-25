d8bda1a61a8aa9442b84afffec28f7dafd9df044
IskyAprilianto <113821298+IskyAprilianto@users.noreply.github.com>
2022-09-25 14:00:19 +0700

    Add files via upload

---
+++ praktikum algoritma minggu ke 1 tugas 1.py
@@ -0,0 +1,12 @@
+#IskyDwiAprilianto
+
+print("nama kamu siapa? : ")
+x = input()
+print("selamat pagi," + x)
+print("darimana asalmu? :")
+y = input()
+print("wah keren dari," + y)
+print("darimana asal sekolahimu? :")
+z = input()
+print("pasti menyenangkan bersekolah di " + z )
+print("senang berkenalan denganmu hehe")
---
+++ praktikum algoritma minggu ke 1 tugas 2.py
@@ -0,0 +1,8 @@
+#IskyDwiAprilianto
+
+print("hitung luas ruangan")
+a = float (input ("masukan panjang ruangan : "))
+b = float (input ("masukan luas ruangan : "))
+c = input ("masukan satuan meter / inci : ")
+Luas=a*b
+print("luas ruangan adalah" + str (Luas)+c)
